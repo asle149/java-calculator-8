c03276df4fb04c9ce2899aeaa50a183055455ade
Kimminsol <gwsak@dgu.ac.kr>
2025-10-19 06:00:45 +0900

    feat: 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,27 +11,54 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
-        //빈 문자열 처리
+        // 빈 문자열 처리
         if(input.equals("")){
             System.out.println("결과 : 0");
             return;
         }
 
-        int ans;
-
-        //커스텀 구분자 확인
+        // 커스텀 구분자 확인
         String delimiterRegex = ",|:";
+        String payload = input;
         if(input.startsWith("//")) {
             int idx = input.indexOf("\n");
-            ///예외처리  if(idx == -1) {}
+            if(idx == -1 || idx == 2) {
+                throw new IllegalArgumentException("잘못된 커스텀 구분자 형식입니다.");
+            }
             String delimiter = input.substring(2, idx);
             delimiterRegex += "|" + Pattern.quote(delimiter);
+            payload = input.substring(idx + 1);
         }
 
+        // 숫자 분리
         List<Integer> nums = new ArrayList<>();
-        String[] tokens = input.split(delimiterRegex);
-        /// 예외처리
-        for(String token : tokens){nums.add(Integer.parseInt(token));}
+        String[] tokens = payload.split(delimiterRegex, -1);
+        for(String token : tokens){
+            validateToken(token);
+            nums.add(Integer.parseInt(token));
+        }
 
+        /// todo: 계산 구현
+
+    }
+
+    private static void validateToken(String token) {
+        // 빈 문자열인지 확인
+        if (token == null || token.isEmpty()) {
+            throw new IllegalArgumentException("구분자 사이 빈 값은 허용되지 않습니다.");
+        }
+
+        // 숫자 형식인지 확인
+        for (char ch : token.toCharArray()) {
+            if (ch < '0' || ch > '9') {
+                throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다.");
+            }
+        }
+
+        // 양수 확인
+        int number = Integer.parseInt(token);
+        if (number <=  0) {
+            throw new IllegalArgumentException("양수만 허용됩니다.");
+        }
     }
 }
